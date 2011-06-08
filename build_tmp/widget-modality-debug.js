c28d94a6ef122b5f7d0886b0e208b6bdb29f7ea9
Tilo Mitra <tilomitra@gmail.com>
2011-06-08 16:20:31 -0700

    changes to nesting code, cleaned it up a bit too

--- build_tmp/widget-modality-debug.js
+++ build_tmp/widget-modality-debug.js
@@ -91,22 +91,15 @@ var WIDGET         = 'widget',
         renderUI : function () {
             
             var bb = this.get(HOST).get(BOUNDING_BOX),
-                cb = this.get(HOST).get(CONTENT_BOX),
-                bbParent = bb.get('parentNode') || Y.one('body');
+                cb = this.get(HOST).get(CONTENT_BOX);
 
             //this makes the content box content appear over the mask
             cb.setStyles({
                 position: "relative"
             });
 
-            //Get the mask off DOM
-            this._maskNode.remove();
-
-            //Insert it back into DOM at the right place
-            bbParent.insert(this._maskNode, bbParent.get('firstChild'));
+            this._repositionMask(this.get(HOST));
             bb.addClass(MODAL_CLASSES.modal);
-
-
 
         },
 
@@ -135,7 +128,8 @@ var WIDGET         = 'widget',
                 oldTI = bb.get('tabIndex');
 
             bb.set('tabIndex', oldTI >= 0 ? oldTI : 0);
-            Y.later(0, host, 'focus');
+            host.focus();
+            //Y.later(0, host, 'focus');
 
             //this._detachUIHandles();
             //host.focus();
@@ -155,17 +149,21 @@ var WIDGET         = 'widget',
         _uiSetHostVisible : function (visible) {
 
             var self = this,
-            id = this.get('host').get('id'),
+            id = this.get(HOST).get('id'),
             len = WidgetModal.STACK.length;
+
+
             //whatever is at the top of the stack receives focus, everything else is blurred.
+            //in this case, the element is visible, so it goes to the top of stack and gets focus
             if (visible) {
 
+                //blur everything in the stack
                 for (var i = 0; i < len; i++) {
                     WidgetModal.STACK[i].modal._detachUIHandles();
                     WidgetModal.STACK[i].modal._blur();
                 }
 
-                //add the current instance onto the stack under the "id" key
+                //push the current instance to top of stack
                 WidgetModal.STACK.push({
                     host: self.get('host'), 
                     modal: self,
@@ -174,8 +172,11 @@ var WIDGET         = 'widget',
 
                 console.log(WidgetModal.STACK);
 
+                //attach ui handles to the current element, show mask, and focus
                 this._attachUIHandles();
-                //Y.later(1, this, '_attachUIHandles');
+
+                this._repositionMask(this.get(HOST));
+                this._uiSetHostZIndex(this.get(HOST).get(Z_INDEX));
                 this._maskNode.setStyle('display', 'block');
                 this._focus();
 
@@ -184,32 +185,39 @@ var WIDGET         = 'widget',
             //if it just lost visibility (was hidden)
             else {
 
-                //take it off the stack and blur / detach stuff from it.
-                for (var j = 0; j < len; j++) {
+                //pop the hidden element off the stack.
+                for (var j = 0; j < WidgetModal.STACK.length; j++) {
                     if (WidgetModal.STACK[j].id === id) {
+
+                        //pop the hidden element off the stack.
                         var o = WidgetModal.STACK.pop();
-                        //delete WidgetModal.STACK[j];
+
+                        console.log(WidgetModal.STACK);
+
+                        //detach UI handles and blur it.
                         o.modal._detachUIHandles();
                         o.modal._blur();
 
                     }
                 }
 
-                len = WidgetModal.STACK.length;
-                
+
                 //if nothing else is in the stack, then hide the mask
-                if (len === 0) {
+                if (WidgetModal.STACK.length === 0) {
                     this._maskNode.setStyle('display', 'none');
                 }
 
-                //otherwise reposition the mask behind the last element in the stack
-                else {
-                    this._maskNode.remove();
-                    len = WidgetModal.STACK.length;
-                    var host = WidgetModal.STACK[len-1].host;
-                    var bbParent = host.get(BOUNDING_BOX).get('parentNode');
-                    bbParent.insert(this._maskNode, bbParent.get('firstChild'));
+
+                //if something else is on the stack, it means it's still visible (behind the element that was just hidden)
+                //in this case, go to the next thing on the stack, and reposition the mask behind it
+                else  {
+
+                    var host = WidgetModal.STACK[WidgetModal.STACK.length - 1].host;
+                    this._repositionMask(host);
+                    host.modal.bindUI();
+                    //host.modal.syncUI();
                     host.modal._uiSetHostZIndex(host.get(Z_INDEX));
+                    host.modal._focus();
                 }
             }
         },
@@ -260,6 +268,15 @@ var WIDGET         = 'widget',
         _isNested: function() {
             var m = WidgetModal._GET_MASK();
             return m.get(VISIBLE);
+        },
+
+        //w is the host behind which mask should be repositioned
+        _repositionMask: function(host) {
+            //get rid of the mask and reposition it behind the last element in the stack
+            this._maskNode.remove();
+            var bb = host.get(BOUNDING_BOX),
+            bbParent = bb.get('parentNode') || Y.one('body');
+            bbParent.insert(this._maskNode, bbParent.get('firstChild'));
         }
 
     }, {
